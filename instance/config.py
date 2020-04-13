dddc17575c00b2de4b37822e4d234b4d59c9da51
Shubham Kumar <kumar.shubham5504@gmail.com>
2020-04-13 14:03:23 -0700

    fixed .gitignore

--- instance/config.py
+++
@@ -1,7 +0,0 @@
-"""
-Private Configuration File.
-"""
-
-DEBUG = True  # Permit use of Flask's debugging features
-SECRET_KEY = '\xe0\xcd\xb0\xe0A\xa1\xf9\x91\xdb\xd4\xc4a\x1d$\x84'
-CLIMACELL_API_KEY = 'XbleX8NlE0oIrxx4vepJHBtOdD42GIeH'

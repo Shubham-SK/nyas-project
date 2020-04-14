55e637c2176e714bc12908ffc11c31baf63e522e
Shubham Kumar <kumar.shubham5504@gmail.com>
2020-04-14 10:29:38 -0700

    yasolike

--- optime/scheduling.py
+++ optime/scheduling.py
@@ -15,9 +15,15 @@ def schedule(start_time, end_time, duration):
     end_time: (ISO 8601 time stamp) ex: 2019-03-20T14:09:50Z
     duration: (num) minutes
     """
-    start_time = dateutil.parser.parse(start_time)
+    if start_time != "now":
+        start_time = dateutil.parser.parse(start_time)
+    else:
+        start_time = dt.now()
+
     end_time = dateutil.parser.parse(end_time)
 
     time_difference = end_time - start_time
 
+    print(time_difference)
+    
     return time_difference

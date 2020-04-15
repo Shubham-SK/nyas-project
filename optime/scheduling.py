f8af27343e29414c0aad4a7674c0f82c08b536c6
Shubham Kumar <kumar.shubham5504@gmail.com>
2020-04-15 02:47:54 -0700

    Organized Climacell and Worked on Scheduling

--- optime/scheduling.py
+++ optime/scheduling.py
@@ -1,14 +1,12 @@
 """
 Uses ClimaCell API to schedule outting.
 """
-from climacell import Weather
-from datetime import datetime as dt
 from datetime import timedelta
-import dateutil.parser
+from datetime import datetime as dt
 import pytz
-import time
+from climacell import Weather
 
-weather = Weather()
+weather = Weather() # pylint: disable=C0103
 
 def schedule(lat, lon, start_time, end_time, duration):
     """
@@ -22,14 +20,13 @@ def schedule(lat, lon, start_time, end_time, duration):
     """
     # calculate time difference to make correct API call
     now_time = dt.utcnow().replace(tzinfo=pytz.utc)
-    start_time_delta = (start_time-now_time).total_seconds()
     total_time_delta = (end_time-start_time).total_seconds()
 
     # make sure paramters are valid
     try:
-        assert(now_time <= start_time < end_time) # chronology
-        assert(end_time <= now_time+timedelta(days=15)) # ensure 15 day scope
-        assert(duration <= (end_time-start_time).total_seconds()) # duration check
+        assert now_time <= start_time < end_time # chronology
+        assert end_time <= now_time+timedelta(days=15) # ensure 15 day scope
+        assert duration <= (end_time-start_time).total_seconds()  # duration check
     except AssertionError:
         print(f'invalid parameters provided.')
 
@@ -42,49 +39,68 @@ def schedule(lat, lon, start_time, end_time, duration):
     weather_data = []
     fields = ['temp', 'humidity', 'humidity:%']
 
-    print(total_time_delta)
+    print(f'total time delta: {total_time_delta} secs')
 
     # populate weather_data array with nowcast
     if total_time_delta > 0 and start_time < nowcast_end:
+        # determine relative bounds
         relative_start_time = start_time
         relative_end_time = min(end_time, nowcast_end)
+        # call API
         data = weather.get_nowcast(
-        lat, lon, 5, 'us', fields,
-        relative_start_time.isoformat(), relative_end_time.isoformat()
+            lat, lon, 5, 'us', fields,
+            relative_start_time.isoformat(), relative_end_time.isoformat()
         )
+        # append data
         weather_data.append(data)
-        total_time_delta -= round((relative_end_time-start_time).total_seconds())
+        # update variables for next timestep
+        total_time_delta -= round(
+            (relative_end_time-start_time).total_seconds()
+        )
         relative_start_time = relative_end_time
 
     print(f'after nowcast: {total_time_delta} secs')
 
     # populate weather_data array with hourly
     if total_time_delta > 0 and relative_start_time < hourly_end:
+        # determine relative bounds
         relative_end_time = min(end_time, hourly_end)
+        # call API
         data = weather.get_hourly(
-        lat, lon, 'us', fields,
-        relative_start_time.isoformat(), relative_end_time.isoformat()
+            lat, lon, 'us', fields,
+            relative_start_time.isoformat(), relative_end_time.isoformat()
         )
+        # append data
         weather_data.append(data)
-        total_time_delta -= round((relative_end_time-relative_start_time).total_seconds())
+        # update variables for next timestep
+        total_time_delta -= round(
+            (relative_end_time-relative_start_time).total_seconds()
+        )
         relative_start_time = relative_end_time
 
     print(f'after hourly: {total_time_delta} secs')
 
     # populate weather_data array with daily
     if total_time_delta > 0 and relative_start_time < daily_end:
+        # determine relative bounds
         relative_end_time = min(end_time, daily_end)
+        # call API
         data = weather.get_daily(
-        lat, lon, 'us', fields,
-        relative_start_time.isoformat(), relative_end_time.isoformat()
+            lat, lon, 'us', fields,
+            relative_start_time.isoformat(), relative_end_time.isoformat()
         )
+        # append data
         weather_data.append(data)
-        total_time_delta -= round((relative_end_time-relative_start_time).total_seconds())
+        # update variables for next timestep
+        total_time_delta -= round(
+            (relative_end_time-relative_start_time).total_seconds()
+        )
 
     print(f'after daily: {total_time_delta} secs')
 
+    # make sure data has been logged for the entire time frame
     try:
-        assert(total_time_delta <= 0)
+        assert total_time_delta <= 0
     except AssertionError:
         print("error in time processing.")
 

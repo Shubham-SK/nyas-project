61d393430983937e4e847199c5181b10e673c763
Shubham Kumar <kumar.shubham5504@gmail.com>
2020-04-18 13:21:21 -0700

    Fixed port

--- optime/app.py
+++ optime/app.py
@@ -134,7 +134,7 @@ def register():
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
-    
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -174,4 +174,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)

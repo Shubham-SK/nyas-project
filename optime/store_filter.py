d1824e764d05b1516dc71c6dbbff2b74b677ef3c
Shubham Kumar <kumar.shubham5504@gmail.com>
2020-04-20 13:53:56 -0700

    added stuff to index for distinguishability

---
+++ optime/store_filter.py
@@ -0,0 +1,19 @@
+"""
+Uses API data from Google and NYTimes to find the safest stores
+"""
+from corona import get_cases
+from places import Stores
+
+STORES = Stores()
+
+def clean_store_data(raw_json):
+    pass
+
+
+def clean_case_data(raw_json):
+    pass
+
+def get_case_data(county):
+    pass
+
+def get_store_data()

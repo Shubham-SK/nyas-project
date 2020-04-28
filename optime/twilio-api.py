edac40de57794d5755bf1719be135a62da2f73e0
Shlok Mehrotra <shlokmehrotra@gmail.com>
2020-04-28 16:10:10 -0700

    umm ya

--- optime/twilio-api.py
+++ optime/twilio-api.py
@@ -1,18 +1,18 @@
 import twilio
 from twilio.rest import Client
+import config
 
 # Your Account SID from twilio.com/console
-account_sid = "ACe183491d933fcd0833afc846d592b726"
+account_sid = config.TWILIO_SID
 # Your Auth Token from twilio.com/console
-auth_token  = "3599256d23ed00dd22eaaa0f36c70745"
+auth_token  = config.TWILIO_AUTH_TOKEN
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
+
     to="+14089967710", 
     from_="+16098628484",
     body="Hello from Python!")
-
-
 
 print(message.sid)
